023845ec442afd3f30ca5f1c4880f56c1b25fd8c
GODhanQ <hg.min2018@gmail.com>
2025-09-30 17:58:49 +0900

    객체지향적 프로그램 만들기

---
+++ Lecture07_Game_Objects/boy_grass_object.py
@@ -0,0 +1,108 @@
+from pico2d import *
+import random
+
+# Game object class here
+
+# 1. 객체를 도출 - 추상화
+# 2. 속성을 도출 - 추상화
+# 3. 행위를 도출
+# 4. 클래스를 제작
+# 5. 객체를 생성
+
+class Grass:
+    def __init__(self):
+        self.image = load_image('grass.png')
+    def update(self):
+        pass
+    def draw(self):
+        self.image.draw(400, 30)
+
+class Boy:
+    def __init__(self):
+        self.x, self.y = random.randint(200, 600), 90
+        self.frame = random.randint(0, 7)
+        self.moving_vec = random.randint(3, 3)
+        self.image = load_image('run_animation.png')
+
+    def update(self):
+        self.frame = (self.frame + 1) % 8
+        self.x += self.moving_vec
+        if self.x > 800:
+            self.x = 0
+
+    def draw(self):
+        self.image.clip_draw(self.frame * 100, 0, 100, 100, self.x, self.y)
+
+class Zombie:
+    def __init__(self):
+        self.x, self.y = 100, 100
+        self.frame = random.randint(0, 7)
+        self.moving_vec = random.randint(3, 3)
+        self.image = load_image('zombie_run_animation.png')
+
+    def update(self):
+        self.frame = (self.frame + 1) % 10
+        self.x += self.moving_vec
+        if self.x > 800:
+            self.x = 0
+
+    def draw(self):
+        frame_width = self.image.w // 10
+        frame_height = self.image.h
+        self.image.clip_draw(self.frame * frame_width, 0,
+                             frame_width, frame_height,
+                             self.x, self.y,
+                             frame_width // 5, frame_height // 5)
+def handle_events():
+    global running
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            running = False
+
+
+
+
+open_canvas()
+
+global team, zombies
+def reset_world():
+    global running
+    running = True
+
+    global world # 모든 게임 객체의 리스트
+    world = [] # 게임 월드 초기화
+
+    # 객체들을 생성
+    global grass, team, zombies
+    grass = Grass()
+    world.append(grass)
+
+    team = [Boy() for _ in range(11)]
+    world += team
+
+    zombies = [Zombie() for _ in range(1)]
+    world += zombies
+
+reset_world()
+
+def update_world():
+    for game_object in world:
+        game_object.update()
+
+def render_world():
+    clear_canvas()
+    for game_object in world:
+        game_object.draw()
+    update_canvas()
+
+while running:
+    handle_events()
+    update_world() # 객체들의 상호작용들을 시뮬레이션 (계산)
+    render_world() # 객체들의 모습을 화면에 그린다.
+
+    delay(0.05)
+
+close_canvas()
